305422fa1eaacdf4bf07702fe42a68189d15dd56
Nishant Patel <nishantpatel@Nishants-MBP.home>
2018-01-27 11:57:17 -0500

    basic proj work

--- hror/hello_app/app/controllers/application_controller.rb
+++ hror/hello_app/app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
+# class ApplicationController < ActionController::Base
+#   protect_from_forgery with: :exception
+# end
+
+# Wafer-thin change to display a hello
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "¡Das na Das!"
+  end
+  def goodbye
+  	render html: "Jay Swaminarayan bhagaaaaaaat!"
+  end
 end
